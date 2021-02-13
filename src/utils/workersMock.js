90e2c2ea2f9eff18b1fbed27062deb81e919c32f
Beto <betoml5@hotmail.com>
2021-02-12 23:55:04 -0600

    Doing workers slider. Just missing details

---
+++ src/utils/workersMock.js
@@ -0,0 +1,29 @@
+const workers = [
+  {
+    name: "Luis",
+    last_name: "Alvarez",
+    rating: 2
+  },
+  {
+    name: "Luis",
+    last_name: "Alvarez",
+    rating: 2
+  },
+  {
+    name: "Luis",
+    last_name: "Alvarez",
+    rating: 2
+  },
+  {
+    name: "Luis",
+    last_name: "Alvarez",
+    rating: 2
+  },
+  {
+    name: "Luis",
+    last_name: "Alvarez",
+    rating: 2
+  },
+];
+
+export default workers;
